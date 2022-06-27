5faf4cf0a78524c01efce696cd0854af5a9d3a1a
Hayet Kumasseh <naaakleh@gmail.com>
2022-06-27 16:38:35 -0400

    commit tree 82d5acd6de6832b8918a1ef917e3a65b47d15add

--- methods.rb
+++ methods.rb
@@ -1,1 +1,36 @@
 # Your code here!
+def greet_programmer
+    puts "Hello, programmer!"
+end
+
+def greet(name)
+    puts"Hello, #{name}!"
+end
+
+def greet_with_default(name = "programmer")
+    puts "Hello, #{name}!"
+end
+
+def add(num1, num2)
+    return(num1 + num2)
+end
+
+# def halve(num)
+#     return nil unless num.class==Integer
+#     num/2
+# end
+
+
+def halve(num)
+    if num % 1 != 0
+        return nil
+    else 
+        return num/2
+    end
+end
+
+# def halve(num)
+#     num % 1 == 0? num/2 :  nil
+# end
+# end
+
